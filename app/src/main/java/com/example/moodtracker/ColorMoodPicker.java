c9df014771d32d854da5cca5e234b62d2e396fa8
rita <saweliewamargharita@yandex.ru>
2022-07-18 21:46:00 +0200

    Added ColorPicker Activity

---
+++ app/src/main/java/com/example/moodtracker/ColorMoodPicker.java
@@ -0,0 +1,126 @@
+package com.example.moodtracker;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.slider.Slider;
+import com.jaredrummler.android.colorpicker.ColorPickerDialog;
+import com.jaredrummler.android.colorpicker.ColorPickerDialogListener;
+import com.jaredrummler.android.colorpicker.ColorShape;
+
+public class ColorMoodPicker extends AppCompatActivity implements ColorPickerDialogListener {
+public Slider moodSlider;
+public String currentMood;
+public TextView moodChosen;
+public Button mainColorPicker;
+public Button saveColorMood;
+public int colorChosen;
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_color_mood_picker);
+
+        //получаем Intent от MainActivity
+        Intent intent = getIntent();
+        moodSlider = findViewById(R.id.moodSlider);
+        mainColorPicker = findViewById(R.id.mainColorPicker);
+        saveColorMood = findViewById(R.id.saveColorMood);
+        //Тут надо бы брать из МейнАктивити цвет тоже, наверное
+        colorChosen = ContextCompat.getColor(ColorMoodPicker.this,R.color.purple_700);
+        //Обьявляем интент из этого активити в MainActivity
+        Intent sendingIntent = new Intent(getApplicationContext(), MainActivity.class);
+        //устанавливаем флаг что-бы стэк активити почистился и нам опять вернуло MainActivity
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        /* у меня был баг с обьявлением ивента onClick в .xml файле в теге кнопки, поєтому лучше прописывать ивенты в коде
+         * вот так */
+        saveColorMood.setOnClickListener(v -> {
+
+            // при нажатии кнопки забираем цвет
+            sendingIntent.putExtra("color",colorChosen);
+            //отправляем интент обратно в mainActivity
+            startActivity(sendingIntent);
+
+        });
+
+    }
+    //Описываем поведение при нажатии кнопки
+    public void onClickColorButton(View view) {
+        switch (view.getId()) {
+            case R.id.mainColorPicker:
+                createColorPickerDialog();
+                break;
+        }
+    }
+    //Функция для перехода на экран настроек
+    private void createColorPickerDialog() {
+        ColorPickerDialog.newBuilder()
+                .setColor(Color.RED)
+                .setDialogType(ColorPickerDialog.TYPE_PRESETS)
+                .setAllowCustom(true)
+                .setAllowPresets(true)
+                .setColorShape(ColorShape.SQUARE)
+                .setDialogId(1)
+                .show(this);
+    }
+    @Override
+    public void onColorSelected(int dialogId, int color) {
+        //Новый цвет присваиваю кнопке
+        Drawable buttonDrawable = mainColorPicker.getBackground();
+        buttonDrawable = DrawableCompat.wrap(buttonDrawable);
+        //the color is a direct color int and not a color resource
+        DrawableCompat.setTint(buttonDrawable, color);
+        mainColorPicker.setBackground(buttonDrawable);
+        colorChosen = color;
+    }
+
+    @Override
+    public void onDialogDismissed(int dialogId) {
+
+    }
+    //Описываем поведение при изменении слайдера, не знаю, как навесить на событие перемещения слайдера
+    public void ChangeSlider(){
+        //Получаю текущее значение у слайдера
+        moodSlider = findViewById(R.id.moodSlider);
+        int moodNumber = (int)moodSlider.getValue();
+        //В зависимости от текущего значения ставлю значение настроения
+        switch (moodNumber){
+            case 0:
+                currentMood = "Severe depressive";
+                break;
+            case 1:
+                currentMood = "Depressive";
+                break;
+            case 2:
+                currentMood = "Sad";
+                break;
+            case 3:
+                currentMood = "Indifferent";
+                break;
+            case 4:
+                currentMood = "Happy";
+                break;
+            case 5:
+                currentMood = "Hypomanic";
+                break;
+            case 6:
+                currentMood = "Manic";
+                break;
+        }
+        //Ставим значение текстового поля в завимости от выбранного настроения
+        moodChosen = findViewById(R.id.moodChosen);
+        moodChosen.setText(currentMood);
+    }
+
+}
--- app/src/main/java/com/example/moodtracker/MainActivity.java
+++ app/src/main/java/com/example/moodtracker/MainActivity.java
@@ -2,34 +2,29 @@ package com.example.moodtracker;
 
 
 
-import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.text.SpannableString;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 
+import com.example.moodtracker.databinding.ActivityMainBinding;
 import com.example.moodtracker.ui.dashboard.CustomActivityChart;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.drawable.DrawableCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import com.example.moodtracker.databinding.ActivityMainBinding;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
-import com.jaredrummler.android.colorpicker.ColorPickerDialog;
-import com.jaredrummler.android.colorpicker.ColorPickerDialogListener;
-import com.jaredrummler.android.colorpicker.ColorShape;
 
-public class MainActivity extends AppCompatActivity implements ColorPickerDialogListener {
+public class MainActivity extends AppCompatActivity  {
     //Кнопка для выбора цвета
     private Button colorPicker;
     private ActivityMainBinding binding;
@@ -106,27 +101,22 @@ public class MainActivity extends AppCompatActivity implements ColorPickerDialog
             workTextView.setText(intent.getStringExtra("work"));
 
         }
-    }
-
-    //Функция для перехода на экран настроек
-
-    private void createColorPickerDialog() {
-        ColorPickerDialog.newBuilder()
-                .setColor(Color.RED)
-                .setDialogType(ColorPickerDialog.TYPE_PRESETS)
-                .setAllowCustom(true)
-                .setAllowPresets(true)
-                .setColorShape(ColorShape.SQUARE)
-                .setDialogId(1)
-                .show(this);
-    }
-
-    public void onClickColorButton(View view) {
-        switch (view.getId()) {
-            case R.id.colorPicker:
-                createColorPickerDialog();
-                break;
+        //Добавила возвращение со страницы настроек цвета
+        else if(intent.hasExtra("color"))
+        {
+            colorPicker = findViewById(R.id.colorPicker);
+            //Новый цвет присваиваю кнопке
+            Drawable buttonDrawable = colorPicker.getBackground();
+            buttonDrawable = DrawableCompat.wrap(buttonDrawable);
+            //Надо проверить, чтобы оно срабатывало((((
+            DrawableCompat.setTint(buttonDrawable, intent.getIntExtra("color", 1));
+            colorPicker.setBackground(buttonDrawable);
         }
+    }
+    public void onClickColorButton(View view) {
+        //Переходим на страницу с настройками цвета
+        Intent intent = new Intent(this, ColorMoodPicker.class);
+        startActivity(intent);
     }
     public void onClickDailyPictureButton(View view) {
         createDailyPicture = findViewById(R.id.dailypicture);
@@ -181,13 +171,5 @@ public class MainActivity extends AppCompatActivity implements ColorPickerDialog
 
         startActivity(intent);
     }
-    @Override
-    public void onColorSelected(int dialogId, int color) {
-        //Здесь нужно написать код, когда тема меняется
-    }
 
-    @Override
-    public void onDialogDismissed(int dialogId) {
-
-    }
 }
